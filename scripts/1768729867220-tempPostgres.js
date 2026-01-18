0479034ad76fefd72ac6e8bcec89d93c7efbb843
kanishk-zluri <kanishk.a@zluri.com>
2026-01-18 15:21:07 +0530

    Upload script: 1768729867220-tempPostgres.js

---
+++ scripts/1768729867220-tempPostgres.js
@@ -0,0 +1,16 @@
+const pg = require("platform/postgres");
+
+async function pingPostgres(db) {
+  const result = await db.query("SELECT name FROM users;");
+  return result.rows;
+}
+
+module.exports = async function main() {
+  const db = await pg("neon_main", "customer_db");
+
+  const result = await pingPostgres(db);
+
+  console.log(result);
+
+  db.release();
+};
