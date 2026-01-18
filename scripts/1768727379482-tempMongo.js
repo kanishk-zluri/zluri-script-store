807165c438cd4da98df08fb607a77874c23ed17b
kanishk-zluri <kanishk.a@zluri.com>
2026-01-18 14:39:40 +0530

    Upload script: 1768727379482-tempMongo.js

---
+++ scripts/1768727379482-tempMongo.js
@@ -0,0 +1,14 @@
+const mongo = require("platform/mongo");
+
+async function pingMongo(mongoDb) {
+  const result = await mongoDb.command({ ping: 1 });
+  return result;
+}
+
+module.exports = async function main() {
+  const mongoDb = await mongo("atlas-instance", "zluri_sales");
+
+  const result = await pingMongo(mongoDb);
+
+  console.log(result);
+};
