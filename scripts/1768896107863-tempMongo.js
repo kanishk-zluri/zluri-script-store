85b667eb099269b7506b04f04f5b743ca403fbdf
kanishk-zluri <kanishk.a@zluri.com>
2026-01-20 13:31:48 +0530

    Upload script: 1768896107863-tempMongo.js

---
+++ scripts/1768896107863-tempMongo.js
@@ -0,0 +1,31 @@
+// const mongo = require("platform/mongo");
+
+// async function pingMongo(mongoDb) {
+//   const result = await mongoDb.command({ ping: 1 });
+//   return result;
+// }
+
+// module.exports = async function main() {
+//   const mongoDb = await mongo( "zluri_sales");
+
+//   const result = await pingMongo(mongoDb);
+
+//   console.log(result);
+// };
+
+const mongo = require("platform/mongo");
+
+module.exports = async function main() {
+  const mongoDb = await mongo("zluri_sales");
+
+  const docs = await mongoDb
+    .collection("sales")
+    .find({ "status": "OPEN" })
+    .limit(10)
+    .toArray();
+
+  console.log("Open sales records:");
+  console.log(docs);
+
+  return docs;
+};
